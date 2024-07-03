b64bd7555633eddec2697113f333d254ee95c4cd
Nadir <bmnadir2@gmail.com>
2024-07-03 15:33:07 +0100

    Switched UI from ttkbootstrap to customtkinter

--- UI/Tk_camera.py
+++ UI/Tk_camera.py
@@ -1,54 +1,51 @@
-import tkinter as tk
-import ttkbootstrap as ttk
+import customtkinter as ctk
 import cv2 
-from PIL import Image, ImageTk 
+from PIL import Image 
 from ultralytics import YOLO
 from cam_capture import get_vcap
 
 ### initial setup
-# Get the four video captures
-caps = [get_vcap (channel = 1), get_vcap (channel = 2), get_vcap (channel = 3), get_vcap (channel = 4)]
 
 # Load YOLO model 
-model = YOLO ('models\\yolov10s.pt') 
+model = YOLO ('models\\yolov10n.pt') 
 
 FRAME_SCALE = .3
+FRAME_WIDTH, FRAME_HEIGHT = (640, 480) 
+FRAME_SIZE = (FRAME_WIDTH, FRAME_HEIGHT)
 RECT_COLOR = (255, 255, 0)
-
-width, height = 800, 600
   
-app = ttk.Window (themename='darkly') 
-app.state('zoomed')
+app = ctk.CTk () 
+app.title ("SMRIS AI Surveillance")
+# app.state('zoomed')
+app._state_before_windows_set_titlebar_color = 'zoomed'
   
 # Bind the app with Escape keyboard to quit app whenever pressed 
 app.bind('<Escape>', lambda e: app.quit()) 
 
 
 ### CAMERA FRAME
-camera_frame = tk.LabelFrame (app, text="Camera Feed", font= ('Calibri 14 bold'))
+camera_frame = ctk.CTkFrame (app) # text="Camera Feed", font= ('Calibri 14 bold')
 camera_frame.pack (side='left', expand=True, fill='y', pady=20, ipady=20)
 
 # Channel combobox 
-channel_var = tk.IntVar (value=1)
-channel_combo = ttk.Combobox (camera_frame, values=[1, 2, 3, 4], textvariable=channel_var)
+channel_var = ctk.StringVar (value="1")
+channel_combo = ctk.CTkComboBox (camera_frame, values=['1', '2', '3', '4'], variable=channel_var, command = lambda event: combo_select (int (channel_var.get ())))
 
-channel_label = ttk.Label (camera_frame, text='Channel ' + str (channel_var.get ()), font='Calibri 18 bold') 
+channel_label = ctk.CTkLabel (camera_frame, text='Channel ' + channel_var.get (), font=('Calibri', 24, 'bold')) 
 channel_label.pack (side='top')
 
-channel_combo.bind ('<<ComboboxSelected>>', lambda event: combo_select (channel_var.get ()))
-
 # Create a label and display it on app 
-cam_label = ttk.Label(camera_frame, width=100) 
+cam_label = ctk.CTkLabel (camera_frame, text='') 
 cam_label.pack(expand=False, fill='x', pady=10, padx=10, side='top') 
 channel_combo.pack (side='top')
 
 # Detection toggle button 
-toggle_var = tk.BooleanVar (value=False)
-detection_toggle = ttk.Checkbutton(camera_frame, text='Toggle people detection', style='info.Roundtoggle.Toolbutton', variable=toggle_var)
+toggle_var = ctk.BooleanVar (value=False)
+detection_toggle = ctk.CTkSwitch(camera_frame, text='Toggle people detection', variable=toggle_var) # style='info.Roundtoggle.Toolbutton'
 detection_toggle.pack (side='top', pady=10)
   
 
-vcap = get_vcap (channel = channel_var.get ())
+vcap = get_vcap (channel = int (channel_var.get ()))
 
 def combo_select (channel): 
     global vcap
@@ -58,11 +55,10 @@ def combo_select (channel):
 
 def open_camera(): 
 
-    # ret, frame = caps[channel_var.get () - 1].read() 
     ret, frame = vcap.read() 
     display_frame = None
     if ret:
-        frame = cv2.resize(frame, (640, 480)) # fx = FRAME_SCALE, fy = FRAME_SCALE
+        frame = cv2.resize(frame, FRAME_SIZE) # fx = FRAME_SCALE, fy = FRAME_SCALE
         # Check if we should detect people or stream raw video frames
         if toggle_var.get ():
             result = model (frame, save=False, verbose=False) ## Inference 
@@ -88,11 +84,9 @@ def open_camera():
         opencv_image = cv2.cvtColor(display_frame, cv2.COLOR_BGR2RGBA) 
     
         # Capture the latest frame and transform to image 
-        captured_image = Image.fromarray(opencv_image) 
+        pil_image = Image.fromarray(opencv_image) 
 
-    
-        # Convert captured image to photoimage 
-        photo_image = ImageTk.PhotoImage(image=captured_image) 
+        photo_image = ctk.CTkImage (dark_image=pil_image, size=FRAME_SIZE)
     
         # Displaying photoimage in the label 
         cam_label.photo_image = photo_image 
