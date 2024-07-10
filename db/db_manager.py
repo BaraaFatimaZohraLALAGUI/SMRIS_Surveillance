374cbbeb91ebcdb74f9f15e8aa40399e8f1493c6
BaraaFatimaZohraLALAGUI <lalaguibaraa@gmail.com>
2024-07-10 08:49:53 +0000

    retrieval by date

--- db/db_manager.py
+++ db/db_manager.py
@@ -1,4 +1,6 @@
 from db.db_config import db 
+import datetime
+from datetime import timezone
 
 def insert_record(record_path, frames_num, timestamp):
     record = {
@@ -12,4 +14,14 @@ def insert_record(record_path, frames_num, timestamp):
 
     return res.inserted_id
 
+def get_record_by_date(date):
+    date = datetime.datetime.strptime(date, "%Y-%m-%d")
+    start_date = datetime.datetime(date.year, date.month, date.day, 0, 0, 0, 000000)
+    end_date = datetime.datetime(date.year, date.month, date.day, 23, 59, 59, 999999)
 
+    return db.records.find({"Timestamp": {'$gte': start_date, '$lte': end_date}})
+
+
+
+def get_record_by_time(start_time, end_time):
+    pass
